02165cd53ce250111f670ee7ddcd89f939779ea3
muroyyy <amirul.faiz01@gmail.com>
2025-12-21 00:24:59 +0800

    feat: add read-only IAM user for monitoring dashboard
    Add monitoring-dashboard-readonly IAM user with CloudWatch and resource metadata read permissions for external monitoring integration

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -67,3 +67,44 @@ resource "aws_iam_user_policy_attachment" "github_actions" {
   user       = aws_iam_user.github_actions.name
   policy_arn = aws_iam_policy.github_actions.arn
 }
+
+# Monitoring Dashboard Read-Only User
+resource "aws_iam_user" "monitoring_readonly" {
+  name = "monitoring-dashboard-readonly"
+}
+
+resource "aws_iam_access_key" "monitoring_readonly" {
+  user = aws_iam_user.monitoring_readonly.name
+}
+
+resource "aws_iam_policy" "monitoring_readonly" {
+  name = "monitoring-dashboard-readonly-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:DescribeAlarms",
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceStatus",
+          "rds:DescribeDBInstances",
+          "lambda:ListFunctions",
+          "lambda:GetFunction",
+          "apigateway:GET",
+          "s3:ListAllMyBuckets",
+          "s3:GetBucketLocation"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "monitoring_readonly" {
+  user       = aws_iam_user.monitoring_readonly.name
+  policy_arn = aws_iam_policy.monitoring_readonly.arn
+}
--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -8,3 +8,14 @@ output "github_actions_secret_access_key" {
   value       = aws_iam_access_key.github_actions.secret
   sensitive   = true
 }
+
+output "monitoring_readonly_access_key_id" {
+  description = "Monitoring dashboard read-only IAM user access key ID"
+  value       = aws_iam_access_key.monitoring_readonly.id
+}
+
+output "monitoring_readonly_secret_access_key" {
+  description = "Monitoring dashboard read-only IAM user secret access key"
+  value       = aws_iam_access_key.monitoring_readonly.secret
+  sensitive   = true
+}
