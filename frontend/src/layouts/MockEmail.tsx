2f3cbfa5f59827289b553ea2fcf3be2f8869c511
muroyyy <amirul.faiz01@gmail.com>
2025-12-29 17:17:26 +0800

    fix frontend CI/CD error

--- frontend/src/layouts/MockEmail.tsx
+++ frontend/src/layouts/MockEmail.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { 
   Mail, 
-  ArrowLeft,
   ExternalLink,
   Clock,
   CheckCircle
